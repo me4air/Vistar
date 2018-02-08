08b34072abdc1f2c5f35a77c02159714ddd3b45f
Vsevolod Vtornikov <me4air@gmail.com>
2018-02-08 12:17:11 +0300

    Добавлен анимированный поиск

--- VistarPasanger/MarshrutViewController.swift
+++ VistarPasanger/MarshrutViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class MarshrutViewController: UIViewController {
+class MarshrutViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchControllerDelegate, UISearchBarDelegate, UISearchResultsUpdating
+  {
 
+    
+    
+
+    
+    @IBOutlet weak var searchBarConstraint: NSLayoutConstraint!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var searchActive : Bool = false
+    var savedSearchConstraint = 0.0
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        savedSearchConstraint = Double(searchBarConstraint.constant)
+        configureSearchController()
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +38,88 @@ class MarshrutViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - TableView
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "TEST"
+        return cell
+    }
+    
+    func configureSearchController() {
+        
+        self.searchController.searchResultsUpdater = self
+        self.searchController.delegate = self
+        self.searchController.searchBar.delegate = self
+        
+        self.searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.dimsBackgroundDuringPresentation = true
+        self.searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Куда поедем?"
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.becomeFirstResponder()
+        searchBar.addSubview(searchController.searchBar)
+        
+    }
+
+    
+    //MARK: Search Bar
+    
+    
+    func openSearch(){
+        UIView.animate(withDuration: 0.4) {
+            self.searchBarConstraint.constant=0
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func closeSearch(){
+        UIView.animate(withDuration: 0.4) {
+            self.searchBarConstraint.constant=CGFloat(self.savedSearchConstraint)
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false
+        closeSearch()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchActive = true
+        openSearch()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false
+        closeSearch()
+    }
+    
+    func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
+        if !searchActive {
+            searchActive = true
+        }
+        
+        searchBar.resignFirstResponder()
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        print ("hi")
+    }
+    
 
 }
